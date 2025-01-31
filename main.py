42a6bfa3707411a9a8b67e77b19180cf5de71bd0
js012108 <js012108@gmail.com>
2025-01-31 13:34:36 -0500

    modulos

---
+++ main.py
@@ -0,0 +1,8 @@
+import ope
+
+num1=float(input('Ingrese el numero 1: '))
+num2=float(input('Ingrese el numero 2: '))
+
+resul=ope.div(num1,num2)
+
+print('El resultado es: ', resul)
---
+++ ope.py
@@ -0,0 +1,14 @@
+def suma(x,y):
+	return x+y 
+
+def resta(x,y):
+	return x-y
+
+def mul(x,y):
+	return x*y 
+
+def div(x,y):
+	if y==0:
+		return "ERROR DIV/0"
+	else:
+		return x/y  
